c02ba0ddcbca83e7cc2649d8b68950fa57fea091
22MH1A05L5 <119724200+22MH1A05L5@users.noreply.github.com>
2022-12-17 10:18:13 +0530

    #include<stdio.h> int main() {     int n,f=1;     scanf("%d",&n);     int i;     for(i=1;i<=n;i++)     {         f=f*i;
        }
        printf("%d",f);
    
    }

---
+++ factorial_of_a_given_Number.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,f=1;
+    scanf("%d",&n);
+    int i;
+    for(i=1;i<=n;i++)
+    {
+        f=f*i;
+            
+        
+        
+    }
+    printf("%d",f);
+
+}

c5aef2a47f6fabbd6936e465fb70e89ee75d1b1b
22MH1A05L5 <119724200+22MH1A05L5@users.noreply.github.com>
2022-12-28 21:00:18 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     if(n<=2)     {         printf("The pattern is not possible");     }     else     {     for(int i=1;i<=n;i++)     {         for(int j=1;j<=i;j++)         {             printf("*");         }         printf(" ");     }     for(int a=n-1;a>=1;a--)     {         for(int c=1;c<=a;c++)         {             printf("*");         }         printf(" ");     }     } }

---
+++ Half_diamond_star_pattern.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n<=2)
+    {
+        printf("The pattern is not possible");
+    }
+    else
+    {
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        printf("
+");
+    }
+    for(int a=n-1;a>=1;a--)
+    {
+        for(int c=1;c<=a;c++)
+        {
+            printf("*");
+        }
+        printf("
+");
+    }
+    }
+}

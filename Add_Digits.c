5fc1cbade38bbefbe080f6fbfc4e4b76bb8c6b22
22MH1A05L5 <119724200+22MH1A05L5@users.noreply.github.com>
2023-01-06 11:08:37 +0530

    #include<stdio.h> int main() {     int n,rem,rem2,sum=0,sum2=0;     scanf("%d",&n);     while(n>9)     {         rem=n%10;         n=n/10;         n=n+rem;     }     printf("%d",n); }

---
+++ Add_Digits.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n,rem,rem2,sum=0,sum2=0;
+    scanf("%d",&n);
+    while(n>9)
+    {
+        rem=n%10;
+        n=n/10;
+        n=n+rem;
+    }
+    printf("%d",n);
+}

ced747d850cb17d47ac120fc97868bc83cc96d4c
22MH1A05L5 <119724200+22MH1A05L5@users.noreply.github.com>
2023-01-06 11:01:18 +0530

    #include<stdio.h> #include<math.h> int main() {   int n,r,rev=0;   scanf("%d",&n);   int sq=n*n;   while(n>0)   {       r=n%10;       rev=rev*10+r;       n=n/10;   }   int sq2=rev*rev;   int rem,rv=0;   while(sq2>0)   {       rem=sq2%10;       rv=rv*10+rem;       sq2=sq2/10;   }   if(sq==rv)   {    printf("True");   }   else   {       printf("False");   } }

--- Adam_number.c
+++ Adam_number.c
@@ -1,30 +1,30 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
-    int n;
-    scanf("%d",&n);
-    int sq=n*n;
-    int t=n,r,rv=0;
-    while(n>0)
-    {
-        r=n%10;
-        rv=rv*10+r;
-        n=n/10;
-    }
-    int sq2=rv*rv;
-    int rem,rev=0;
-    while(sq2!=0)
-    {
-        rem=sq2%10;
-        rev=rev*10+rem;
-        sq2=sq2/10;
-    }
-    if(rev==sq)
-    {
-        printf("True");
-    }
-    else
-    {
-        printf("False");
-}
+  int n,r,rev=0;
+  scanf("%d",&n);
+  int sq=n*n;
+  while(n>0)
+  {
+      r=n%10;
+      rev=rev*10+r;
+      n=n/10;
+  }
+  int sq2=rev*rev;
+  int rem,rv=0;
+  while(sq2>0)
+  {
+      rem=sq2%10;
+      rv=rv*10+rem;
+      sq2=sq2/10;
+  }
+  if(sq==rv)
+  {
+   printf("True");   
+  }
+  else
+  {
+      printf("False");
+  }
 }

be808d572699e6b638349f44cce7207da474d0b6
22MH1A05L5 <119724200+22MH1A05L5@users.noreply.github.com>
2023-01-10 22:34:18 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     for(int i=1;i<=n;i++)     {         for(int k=1;k<=n-i;k++)             {                 printf(" ");             }             for(int j=i-1;j>=1;j--)             {                 printf("%d",j);             }             for(int t=0;t<=i-1;t++)             {                 printf("%d",t);             }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -5,17 +5,18 @@ int main()
     scanf("%d",&n);
     for(int i=1;i<=n;i++)
     {
-        for(int j=1;j<=i;j++)
-        {
-            if(j==1||j==i||i==n||j==n)
-            {
-                printf("*");
-            }
-            else
+        for(int k=1;k<=n-i;k++)
             {
                 printf(" ");
             }
-        }
+            for(int j=i-1;j>=1;j--)
+            {
+                printf("%d",j);
+            }
+            for(int t=0;t<=i-1;t++)
+            {
+                printf("%d",t);
+            }
         printf("
 ");
     }
